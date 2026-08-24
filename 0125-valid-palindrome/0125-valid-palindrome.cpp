e563e567d2948b09ad3e5a43b02362f55d846ad4
Himanshu Chandlani <himanshuchandlani07@gmail.com>
2026-08-24 20:24:06 +0530

    Time: 3 ms (28.2%), Space: 10.3 MB (23.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(char &c:s){
+            c = tolower(c);
+        }
+        string check;
+        for(char c: s){
+            if(isalnum(c)){
+                check.push_back(c);
+            }
+        }
+        reverse(check.begin(),check.end());
+        int left = 0;
+        int right = check.size() - 1;
+        while(left <= right){
+            if(check[left] != check[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
