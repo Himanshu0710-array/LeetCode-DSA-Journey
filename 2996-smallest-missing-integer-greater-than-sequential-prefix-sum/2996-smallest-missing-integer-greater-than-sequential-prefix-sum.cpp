f9d3a03df883c26164c7efbaceb7b1703e6fe9ce
Himanshu Chandlani <himanshuchandlani07@gmail.com>
2026-08-26 22:14:53 +0530

    Time: 0 ms (100%), Space: 22.6 MB (75.39%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int n = nums.size();
+        int sum = nums[0];
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1] == nums[i] + 1){
+                sum += nums[i+1];
+            }else{
+                break;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == sum){
+                sum += 1;
+                i = -1;
+            }
+        }
+        return sum;
+    }
+};
