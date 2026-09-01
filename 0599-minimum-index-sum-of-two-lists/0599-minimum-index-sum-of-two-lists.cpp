0ddaba7928cf7f9cfe1cc7caac04aba2816f058b
Himanshu Chandlani <himanshuchandlani07@gmail.com>
2026-09-01 17:22:46 +0530

    Time: 7 ms (97.85%), Space: 34.5 MB (93.13%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -4,9 +4,15 @@ public:
         int n = list1.size();
         int m = list2.size();
         vector<string> ans;
-        int min = -1;
+        int min = n+m+1;
         for(int i=0;i<n;i++){
+            if(i>min){
+                break;
+            }
             for(int j = 0;j<m;j++){
+                if(i+j > min){
+                    break;
+                }
                 if(list1[i] == list2[j]){
                     if(ans.empty()){
                         ans.push_back(list1[i]);
@@ -19,7 +25,9 @@ public:
                         ans.push_back(list1[i]);
                     }
                 }
+
             }
+
         }
         return ans;
     }
